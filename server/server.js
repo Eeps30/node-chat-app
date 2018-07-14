9c1855d3e650543a14c26fc1d1f4952d8fb7fc90
Eeps30 <eeps30@gmail.com>
2018-07-13 19:02:05 -0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+
+//this module helps to change the path that the express 
+//server takes so that instead of going into 'server', back out of 
+//'server' and then into 'public', it starts up at 'node-chat-app'
+//and then straight into public.
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(3000, () => {
+    console.log(`Server is up on ${port}`);
+})
