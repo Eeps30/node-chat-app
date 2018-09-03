313986501ecfc974ef05da749175f031534fd439
Evan Ippolito <eeps30@gmail.com>
2018-09-03 15:53:10 -0700

    printing coordinates in chat app when button is pressed

--- public/js/index.js
+++ public/js/index.js
@@ -26,3 +26,20 @@ jQuery('#message-form').on('submit', function (event) {
 
     })
 })
+
+var locationButton = jQuery('#send-location');
+
+locationButton.on('click', function () {
+    if (!navigator.geolocation) {
+        return alert('Geolocation not Supported by your Browser')
+    }
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    }, function () {
+        alert('Unable to Fetch Location');
+    })
+})
