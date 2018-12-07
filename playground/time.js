cc77f1bc392fc37a1a713723a17ff05a77d7cf7c
Eeps30 <eeps30@gmail.com>
2018-12-07 15:32:59 -0800

    added time stamps to user message and share location message

--- playground/time.js
+++ playground/time.js
@@ -2,5 +2,13 @@ var moment = require('moment');
 
 // Jan 1st 1970 00:00:10 am
 
-var date = moment();
-console.log(date.format('MMM Do YYYY'));
+// var date = moment();
+// console.log(date.format('MMM Do YYYY'));
+
+// 10:35 am
+// var date = moment();
+// console.log(date.format('h:ma'))
+
+var createdAt = 1234;
+var date = moment(createdAt);
+console.log(date.format('h:mm a'))
